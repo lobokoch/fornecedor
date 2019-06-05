7a0fc15ef79e77c6c568638dcbbd5270b7994464
Márcio Koch <lobokoch@gmail.com>
2019-06-05 06:40:07 -0300

    Removido gitignore lixo

--- web/src-gen/app/modules/cadastros/fornecedor/fornecedor/fornecedor.module.ts
+++ web/src-gen/app/modules/cadastros/fornecedor/fornecedor/fornecedor.module.ts
@@ -1,6 +1,6 @@
 /**********************************************************************************************
 Code generated with MKL Plug-in version: 3.6.2
-Code generated at time stamp: 2019-06-05T06:31:56.876
+Code generated at time stamp: 2019-06-05T06:39:17.496
 Copyright: Kerubin - logokoch@gmail.com
 
 WARNING: DO NOT CHANGE THIS CODE BECAUSE THE CHANGES WILL BE LOST IN THE NEXT CODE GENERATION.
