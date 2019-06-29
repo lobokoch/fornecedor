d23b0fed39b3f549cc8c29618bff5ddf7d7caceb
Márcio Koch <lobokoch@gmail.com>
2019-06-29 12:15:24 -0300

    Adicionados novos campos

--- modules/app/src/main/resources/db/migration/cadastros/fornecedor/PostgreSql/V1__Creation_Tables_PostgreSQL.sql
+++ modules/app/src/main/resources/db/migration/cadastros/fornecedor/PostgreSql/V1__Creation_Tables_PostgreSQL.sql
@@ -2,17 +2,24 @@
 DROP TABLE IF EXISTS fornecedor CASCADE;
 **********************************************************/
 
-CREATE TABLE fornecedor (
+CREATE TABLE fornecedor /* Fornecedor */  (
 	id UUID NOT NULL,
 	nome VARCHAR(255) NOT NULL,
 	cpf_cnpj VARCHAR(255) /* cpfCNPJ */,
-	ie VARCHAR(255),
+	ie_rg VARCHAR(255) /* ieRG */,
 	nome_contato VARCHAR(255) /* nomeContato */,
 	fone VARCHAR(255),
 	celular VARCHAR(255),
 	email VARCHAR(255),
 	site VARCHAR(255),
-	observacoes VARCHAR(1000)
+	cep VARCHAR(255) /* CEP */,
+	cidade VARCHAR(255),
+	bairro VARCHAR(255),
+	endereco VARCHAR(255),
+	numero VARCHAR(255),
+	complemento VARCHAR(255),
+	observacoes VARCHAR(1000),
+	ativo BOOLEAN DEFAULT true
 );
 
 /* PRIMARY KEYS */
